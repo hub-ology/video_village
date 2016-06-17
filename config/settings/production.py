1918c055fb5356fb6f69f2db6c109bbac00eedaf
Brian Painter <brian.m.painter@gmail.com>
2016-06-16 22:41:52 -0400

    trying to setup allowed_hosts correctly

--- config/settings/production.py
+++ config/settings/production.py
@@ -58,17 +58,15 @@ SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
 # ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost', 'video-villiage-dev.us-east-1.elasticbeanstalk.com'])
 
-ALLOWED_HOSTS = [
-    '.compute-1.amazonaws.com',
-]
-EC2_PRIVATE_IP = None
-try:
-    EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01).text
-except requests.exceptions.RequestException:
-    pass
-
-if EC2_PRIVATE_IP:
-    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
+ALLOWED_HOSTS = ['video-village-dev.us-east-1.elasticbeanstalk.com']
+# EC2_PRIVATE_IP = None
+# try:
+#     EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01).text
+# except requests.exceptions.RequestException:
+#     pass
+#
+# if EC2_PRIVATE_IP:
+#     ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
 
 # ALLOWED_HOSTS = ['localhost', 'video-villiage-dev.us-east-1.elasticbeanstalk.com']
 # END SITE CONFIGURATION
