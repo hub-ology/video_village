ea6f89e46d6594b63b8924a826ab4c553606cf78
Brian Painter <brian.m.painter@gmail.com>
2016-06-16 21:55:23 -0400

    added fields to the Video model

--- videos/models.py
+++ videos/models.py
@@ -3,15 +3,19 @@ from django.core.urlresolvers import reverse
 
 
 class Video(models.Model):
-    name = models.CharField(max_length=255)
+    uploader_name = models.CharField(max_length=255, null=False, blank=True)
     file = models.FileField()
+    title = models.CharField(max_length=255, null=False, blank=True)
+    description = models.TextField(null=False, blank=True)
+    email = models.EmailField()
+    approved = models.BooleanField(default=False)
 
     @property
     def video_length(self):
-        return len(self.name)
+        return len(self.title)
 
     def __str__(self):
-        return ''.join(('Video: ', self.name))
+        return ''.join(('Video: ', self.title, ' ', self.uploader_name))
 
     def get_absolute_url(self):
         return reverse('api:video-detail', kwargs={'pk': self.pk})
