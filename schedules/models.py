5ccb6ee67ce98a7b54080e3dc110a054ecc288c9
Brian Painter <brian.m.painter@gmail.com>
2016-07-07 21:01:42 -0400

    migration files for the Pi models and the Schedules Models

--- schedules/models.py
+++ schedules/models.py
@@ -1,17 +1,29 @@
 from django.db import models
 
 
+
 class Schedule(models.Model):
     schedule_date = models.DateField()
-    pi = models.PositiveIntegerField()
+    window = models.ForeignKey('pis.Window')
 
     def __str__(self):
         return self.schedule_date.strftime('%Y-%m-%d') + '  @ Pi:' + str(self.pi)
+
+    class Meta:
+        unique_together = (('schedule_date', 'window'),)
+
+    def duplicate(self, new_date, new_window):
+        """
+        Duplicate the Schedule to a new date and/or time
+        :param new_date:
+        :param new_pi:
+        :return:
+        """
 
 
 class ScheduleItem(models.Model):
     schedule = models.ForeignKey(Schedule, related_name='items')
     video = models.ForeignKey('videos.Video')
     video_start_seconds = models.PositiveIntegerField(null=True, blank=True)
-    video_stop_seconds = models.PositiveIntegerField(null=True, blank=True)
+    video_duration_seconds= models.PositiveIntegerField(null=True, blank=True)
     start_time = models.TimeField()
