28ca4bc1f7fd90b2c2af4240d1744e91e037ba69
Brian Painter <brian.m.painter@gmail.com>
2016-07-09 15:57:04 -0400

    moving the Window model to the Schedule app. Talked with the folks who will do scheduling, and they want a start/end date for each schedule, as a schedule will typically run for 1+ week

--- pis/models.py
+++ pis/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
+
 class Pi(models.Model):
     mac_address = models.CharField(max_length=255)
-
-class Window(models.Model):
-    building = models.PositiveIntegerField()
-    pi = models.ForeignKey(Pi, null=True, blank=True)
--- schedules/models.py
+++ schedules/models.py
@@ -1,22 +1,28 @@
 from django.db import models
 
 
+class Window(models.Model):
+    building = models.PositiveIntegerField()
+    pi = models.ForeignKey('pis.Pi', null=True, blank=True)
+
 
 class Schedule(models.Model):
-    schedule_date = models.DateField()
-    window = models.ForeignKey('pis.Window')
+    schedule_start_date = models.DateField()
+    schedule_end_date = models.DateField()
+    window = models.ForeignKey(Window)
 
     def __str__(self):
         return self.schedule_date.strftime('%Y-%m-%d') + '  @ Pi:' + str(self.pi)
 
     class Meta:
-        unique_together = (('schedule_date', 'window'),)
+        unique_together = (('schedule_start_date', 'window'),)
 
-    def duplicate(self, new_date, new_window):
+    def duplicate(self, new_start_date, new_end_date, new_window):
         """
         Duplicate the Schedule to a new date and/or time
-        :param new_date:
-        :param new_pi:
+        :param new_start_date:
+        :param new_end_date:
+        :param new_window:
         :return:
         """
 
@@ -25,5 +31,11 @@ class ScheduleItem(models.Model):
     schedule = models.ForeignKey(Schedule, related_name='items')
     video = models.ForeignKey('videos.Video')
     video_start_seconds = models.PositiveIntegerField(null=True, blank=True)
-    video_duration_seconds= models.PositiveIntegerField(null=True, blank=True)
-    start_time = models.TimeField()
+    video_duration_seconds = models.PositiveIntegerField(null=True, blank=True)
+    play_order = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = (('schedule', 'play_order'),)
+
+    def __str__(self):
+        return str(self.play_order) + ': ' + str(self.video)
