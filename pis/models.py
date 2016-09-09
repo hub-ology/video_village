3bbeb179604cd1b71f6e0585663d535f0a0fda40
Brian Painter <brian.m.painter@gmail.com>
2016-09-09 08:45:02 -0400

    add status endpoint /api/pis/<pk>/status to store the status returned from the pi. Add a model field to hold this data.

--- pis/models.py
+++ pis/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Pi(models.Model):
     mac_address = models.CharField(max_length=255)
+    status = models.TextField(null=False, blank=True)
 
     def __str__(self):
         return "Pi {}: {}".format(self.id, self.mac_address)
