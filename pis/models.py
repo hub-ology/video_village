39cd47254b3568643a387cc01133eb35d0cba9ca
Brian Painter <brian.m.painter@gmail.com>
2016-09-21 22:12:18 -0400

    add NGROK user/token info to the configuration
    model methods on the Pi to turn the projector on/off and get status
    
    window detail page with buttons to call the Pi status, projector on/off
    
    add Model for CacheFile (storing the file names that are on the Pi)

--- pis/models.py
+++ pis/models.py
@@ -3,6 +3,7 @@ import json
 
 import requests
 from django.db import models
+from django.conf import settings
 
 
 class Pi(models.Model):
@@ -14,6 +15,7 @@ class Pi(models.Model):
     projector_on = models.BooleanField(default=False)
     playlist_active = models.BooleanField(default=False)
     cpu_temp = models.DecimalField(max_digits=6, decimal_places=3, null=True, blank=True)
+    ssh_tunnel = models.CharField(max_length=255, null=False, blank=True)
 
     def __str__(self):
         return "Pi {}: {}".format(self.id, self.mac_address)
@@ -45,13 +47,32 @@ class Pi(models.Model):
             except:
                 pass
 
+            try:
+                self.cachefile_set.all().delete()
+                file_cache = status_data.get('file_cache')
+                for f in file_cache:
+                    file = CacheFile(pi=self, file_name=f)
+                    file.save()
+            except:
+                pass
+
+
     def turn_projector_on(self):
         if self.tunnel:
-            r = requests.get(self.tunnel + '/projector/on')
+            r = requests.post(self.tunnel + '/projector/on', auth=(settings.NGROK_AUTH_USER, settings.NGROK_AUTH_TOKEN))
             return r.json()
 
     def turn_projector_off(self):
         if self.tunnel:
-            r = requests.get(self.tunnel + '/projector/off')
+            r = requests.post(self.tunnel + '/projector/off', auth=(settings.NGROK_AUTH_USER, settings.NGROK_AUTH_TOKEN))
             return r.json()
 
+    def get_status(self):
+        if self.tunnel:
+            r = requests.get(self.tunnel + '/status', auth=(settings.NGROK_AUTH_USER, settings.NGROK_AUTH_TOKEN))
+            return r.json()
+
+
+class CacheFile(models.Model):
+    pi = models.ForeignKey(Pi)
+    file_name = models.CharField(max_length=255)
