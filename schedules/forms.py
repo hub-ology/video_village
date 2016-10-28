52a04c55b2726cb51c964755a0267b51ab44d59b
Brian Painter <brian.m.painter@gmail.com>
2016-10-27 22:55:42 -0400

    set the Window choices during the __init__ to work properly when database is just being created

--- schedules/forms.py
+++ schedules/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 
-from schedules.models import Window
+from .models import Window
 
 
 class StreamForm(forms.Form):
     stream_url = forms.URLField()
-    windows = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, label='Windows to Stream to', choices=[(w.id, w) for w in Window.objects.all()])
+    windows = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, label='Windows to Stream to', choices=[])
+
+    def __init__(self):
+        super(StreamForm, self).__init__()
+        self.fields['windows']['choices'] = [(w.id, w) for w in Window.objects.all()]
